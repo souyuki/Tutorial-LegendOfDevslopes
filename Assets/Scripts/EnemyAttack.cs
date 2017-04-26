fa88ef011dc6109c23c98b77a38b6907f662ac10
Covina <jabs303@gmail.com>
2017-04-26 08:45:32 -0700

    Section 5, Lecture 64;  Created GameManager, EnemyAttack scripts;  Made correction to Solider Sword Box Collider to addess negatiev scale warning.

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour {
+
+	// attack distance
+	[SerializeField] private float attackRange = 3f;
+
+	// The amount of time between attacks
+	[SerializeField] private float timeBetweenAttacks = 1f;
+
+	// Animator to play the attack animations
+	private Animator animator;
+
+	// store the player
+	private GameObject player;
+
+	// check if player is in attack range
+	private bool isPlayerInRange;
+
+	// Store all the weapon colliders (for dual wielders)
+	private BoxCollider[] weaponColliders;
+
+
+	// Use this for initialization
+	void Start () {
+
+		// get the animator component
+		animator = GetComponent<Animator>();
+
+		// get the weapon colliders buried in the hierarchy
+		weaponColliders = GetComponentsInChildren<BoxCollider>();
+
+		// get the player from GameManager
+		player = GameManager.instance.Player;
+
+		// Start the recursive enemy attack coroutine
+		StartCoroutine( Attack() );
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		// calculate distance between enemy and the player
+		if ( Vector3.Distance (transform.position, player.transform.position) < attackRange ) {
+			// player is in range
+			isPlayerInRange = true;
+		} else {
+			isPlayerInRange = false;
+		}
+
+		//Debug.Log("isPlayerInRange: " + isPlayerInRange);
+
+
+	}
+
+
+	// Enemy Attack Process
+	IEnumerator Attack ()
+	{
+
+		// check if player is in range and the game is not over
+		if (isPlayerInRange && !GameManager.instance.IsGameOver) {
+
+			// play the attack animation
+			animator.Play("Attack");
+
+			// time between attacks
+			yield return new WaitForSeconds(timeBetweenAttacks);
+
+		}
+
+		yield return null;
+
+		StartCoroutine( Attack() );
+
+	}
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// create singleton variable
+	public static GameManager instance = null;
+
+	[SerializeField] GameObject player;
+
+	// keep track if game is over
+	private bool isGameOver = false;
+
+	// GETTER for isGameOver
+	public bool IsGameOver {
+		get {
+			return isGameOver;
+		}
+		set {
+			isGameOver = value;
+		}
+	}
+
+	// GETTER for player
+	public GameObject Player {
+		get {
+			return player;
+		}
+		set {
+			player = value;
+		}
+	}
+
+
+	void Awake ()
+	{
+
+
+		// Create singleton
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Check if player is still alive
+	public void PlayerHit (int currentHP)
+	{
+
+		if (currentHP > 0) {
+			// player is alive
+			isGameOver = false;
+		} else {
+			// player is dead
+			isGameOver = true;
+		}
+
+
+	}
+
+
+}
