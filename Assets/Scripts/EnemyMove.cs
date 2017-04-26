c23f35c07511e518c6bfe93aefba0b5a6f4a2c33
Covina <jabs303@gmail.com>
2017-04-25 22:10:51 -0700

    Section 5, Lecture 62;  Animations on hero and enemies; Navmesh and NavMeshAgent hooked up on enemies.

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+
+public class EnemyMove : MonoBehaviour {
+
+	[SerializeField] private Transform player;
+
+	private NavMeshAgent nav;
+	private Animator animator;
+
+
+	void Awake()
+	{
+
+		Assert.IsNotNull(player);
+
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+		animator = GetComponent<Animator>();
+		nav = GetComponent<NavMeshAgent>();
+
+				
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// set pathfinding for the enemies to go after the player
+		nav.SetDestination(player.position);
+
+	}
+}
